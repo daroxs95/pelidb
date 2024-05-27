a57d6ebd09978ce5d8b048f319dbc50a27e77ad7
Darián López Utra <daroxs95@gmail.com>
2024-05-27 23:33:13 +0200

    Fix initial movies list

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,54 @@
-import type { MetaFunction } from "@remix-run/node";
+import type {MetaFunction} from "@remix-run/node";
 import {useLoaderData} from "@remix-run/react";
 import {getMovies} from "~/modules/Movies/infrastructure/data";
 import {List} from "~/modules/Movies/application/List/List";
 import {Movie} from "~/modules/Movies/domain/types";
 import {useEffect, useState} from "react";
 import {Filters} from "~/modules/Movies/application/Filters";
-import { useLocation } from "@remix-run/react";
+import {useLocation} from "@remix-run/react";
 
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "PeliDB" },
-    { name: "description", content: "Top 250 movies list" },
-  ];
+    return [
+        {title: "PeliDB"},
+        {name: "description", content: "Top 250 movies list"},
+    ];
 };
 
 export async function loader() {
-  return getMovies();
+    return getMovies();
 }
 
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
-  const [filteredMovies, setFilteredMovies] = useState<Movie[]>(data);
-  const location = useLocation();
+    const data = useLoaderData<typeof loader>();
+    const location = useLocation();
+    const initialFilters = new URLSearchParams(location.search).get('s');
+    const [filteredMovies, setFilteredMovies] =
+        useState<Movie[]>(data.filter(
+            (movie) =>
+                movie.name.toLowerCase().includes((initialFilters || "").toLowerCase()
+                ))
+        )
+    ;
 
-  const initialFilters = new URLSearchParams(location.search).get('s');
 
-  const onFilterChange = (str: string) => {
-    setFilteredMovies(data.filter((movie) => movie.name.toLowerCase().includes(str.toLowerCase())));
-    const searchParams = new URLSearchParams(location.search);
-    searchParams.set('s', str);
-    history.replaceState(null, '', `${location.pathname}?${searchParams.toString()}`);
-  }
-
-  useEffect(() => {
-    if (initialFilters) {
-      onFilterChange(initialFilters);
+    const onFilterChange = (str: string) => {
+        setFilteredMovies(data.filter((movie) => movie.name.toLowerCase().includes(str.toLowerCase())));
+        const searchParams = new URLSearchParams(location.search);
+        searchParams.set('s', str);
+        history.replaceState(null, '', `${location.pathname}?${searchParams.toString()}`);
     }
-  }, []);
 
-  return (
-    <main>
-      <List movies={filteredMovies} />
-      <Filters onChange={onFilterChange} value={initialFilters || ""} />
-    </main>
-  );
+    useEffect(() => {
+        if (initialFilters) {
+            onFilterChange(initialFilters);
+        }
+    }, []);
+
+    return (
+        <main>
+            <List movies={filteredMovies}/>
+            <Filters onChange={onFilterChange} value={initialFilters || ""}/>
+        </main>
+    );
 }
